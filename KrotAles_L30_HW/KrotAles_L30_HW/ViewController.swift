762d113bc58258af786ef27267b1b50d5aa6765c
Aliaksandr <45662473+AlesKrot@users.noreply.github.com>
2022-02-02 16:40:06 +0300

    Add: create new project

---
+++ KrotAles_L30_HW/KrotAles_L30_HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KrotAles_L30_HW
+//
+//  Created by Ales Krot on 2.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
